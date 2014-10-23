d72b88d530553b812ec47977c6d4b9c4e12c7957
Alex Gwozda <gwozda@gmail.com>
2014-10-23 11:03:13 -0700

    Completed Lesson 1 with work, bio, project, education objects

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,27 +1,92 @@
 var bio = {
   "name" : "Alex Gwozda",
   "role" : "Web Developer",
-  "mobile" : "415-429-2525",
-  "email" : "gwozda@gmail.com",
-  "github" : "https://github.com/alexgwozda",
-  "bioPic" : "images/photo1.png",
   "welcomeMsg" : "Welcome to my interactive resume!",
-  "skills" : ["HTML", "CSS", "Javascript"],
+  "contacts" : [ 
+    {
+      "mobile" : "415-429-2525",
+      "email" : "gwozda@gmail.com",
+      "github" : "https://github.com/alexgwozda",
+      "bioPic" : "images/photo1.png",
+    } 
+  ],
+  "skills" : ["HTML", "CSS", "Javascript"]
 }
 
-var work1 = {};
-work1.employer = "Massage Envy";
-work1.title = "massage therapist";
-work1.dates = "August 2014 - Present";
-work1.location = "Castro Valley, CA";
-work1.description = "Providing massage therapy to relieve stress and pain, as well as to treat injuries and chronic conditions.";
+var work = {
+  "jobs" : [
+    {
+      "employer" : "Massage Envy",
+      "title" : "massage therapist",
+      "dates" : "August 2014 - Present",
+      "location" : "Castro Valley, CA",
+      "description" : "Providing massage therapy to relieve stress and pain, as well as problem-solving to treat injuries and chronic conditions."
+    },
+    {
+      "employer" : "Elements Massage",
+      "title" : "massage therapist",
+      "dates" : "April 2012 - June 2014",
+      "location" : "Seattle, WA",
+      "description" : "Providing massage therapy to relieve stress and pain, as well as problem-solving to treat injuries and chronic conditions."
+    }
+  ]
+}
 
-var school1 = {};
-school1["name"] = "Everest College, Seattle";
-school1["degree"] = "Massage Therapy";
-school1["dates"] = "July 2009 - March 2011";
-school1["location"] = "Seattle, WA";
+var education = {
+  "schools" : [
+    {
+      "name" : "Everest College, Seattle",
+      "degree" : "Massage Therapy",
+      "dates" : "July 2009 - March 2011",
+      "location" : "Seattle, WA"
+    },
+    {
+      "name" : "University of Washington",
+      "majors" : ["BS Biology", "BA Sociology"],
+      "dates" : "September 2005 - March 2010",
+      "location" : "Seattle, WA"
+    }
+  ], 
+  "onlineCourses" : [
+    {
+      "title" : "Front-End Web Development Nanodegree",
+      "school" : "Udacity.com",
+      "dates" : "October 2014 - Present",
+      "url" : "https://www.udacity.com/course/nd001"
+    },
+    {
+      "title" : "Become a Certified Web Developer",
+      "school" : "LearnToProgram via Udemy.com",
+      "dates" : "June 2014 - July 2014",
+      "url" : "https://www.udemy.com/become-a-certified-web-developer/"
+    },
+    {
+      "title" : "A Smarter Way to Learn Javascript",
+      "school" : "..........AUTHOR...........",
+      "dates" : "July 2014 - August 2014",
+      "url" : "http://www.amazon.com/dp/B00H1W9I6C"
+    }
+  ]
+}
 
+var projects = {
+  "projects" : [
+    {
+      "title" : "Responsive Portfolio",
+      "dates" : "October 2014",
+      "description" : "HTML and CSS create a fully responsive portfolio page.",
+      "image" : [ "..............URL...........", 
+                  ".............URL2...........",
+                  ".............URL3..........."
+                ]
+    }
+  ]
+}
+
+
+
+
+/*
 // f stands for formatted
 // Construct the full skills HTML called below.
 function listSkills(skills) {
@@ -81,3 +146,5 @@ $("#school1").append(fSchool1Name);
 $("#school1").append(fSchool1Degree);
 $("#school1").append(fSchool1Dates);
 $("#school1").append(fSchool1Location);
+
+*/
